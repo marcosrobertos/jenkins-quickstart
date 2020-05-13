31371c89c5f8d221410b393f442066fb50f0bd5f
Marcos Roberto <marcos.roberto.lopez@gmail.com>
2020-05-12 22:39:09 -0400

    first commit

---
+++ src/main/java/com/rhiscom/jenkins/quickstart/MathOperations.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rhiscom.jenkins.quickstart;
+
+/**
+ *
+ * @author Roberto Lopez
+ */
+public class MathOperations {
+   
+   public int sum(int a, int b){
+      return a + b;
+   }
+   
+   public int rest(int a, int b){
+      return a - b;
+   }
+}
---
+++ src/test/java/com/rhiscom/jenkins/quickstart/MathOperationsTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rhiscom.jenkins.quickstart;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Roberto Lopez
+ */
+public class MathOperationsTest {
+   
+   public MathOperationsTest() {
+   }
+   
+   @BeforeClass
+   public static void setUpClass() {
+   }
+   
+   @AfterClass
+   public static void tearDownClass() {
+   }
+   
+   @Before
+   public void setUp() {
+   }
+   
+   @After
+   public void tearDown() {
+   }
+
+   @Test
+   public void testSum() {
+      System.out.println("sum");
+      int a = 5;
+      int b = 3;
+      MathOperations instance = new MathOperations();
+      int expResult = 8;
+      int result = instance.sum(a, b);
+      assertEquals(expResult, result);
+   }
+
+   @Test
+   public void testRest() {
+      System.out.println("rest");
+      int a = 10;
+      int b = 3;
+      MathOperations instance = new MathOperations();
+      int expResult = 7;
+      int result = instance.rest(a, b);
+      assertEquals(expResult, result);
+   }
+   
+}
